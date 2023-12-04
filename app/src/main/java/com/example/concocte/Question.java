c8262371c169eaf587f180c58306c97f52233c8b
Joachim <ageron.joachim@gmail.com>
2023-12-04 11:29:24 +0100

    ajout remplissage aléatoire

--- app/src/main/java/com/example/concocte/Question.java
+++ app/src/main/java/com/example/concocte/Question.java
@@ -1,36 +1,46 @@
 package com.example.concocte;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Question {
-    private String question;
-    private String explication;
-    private String answer;
-    private String[] choices;
+private String question;
+private String explication;
+private String answer;
+private String[] choices;
 
-    public Question(String question, String answer, String explication, String[] choices) {
-        this.question = question;
-        this.answer = answer;
-        this.explication = explication;
-        this.choices = choices;
-    }
+public Question(String question, String answer, String explication, String[] choices) {
+	this.question = question;
+	this.answer = answer;
+	this.explication = explication;
+	this.choices = shuffleChoices(choices);
+}
 
-    public String getQuestion() {
-        return question;
-    }
+private String[] shuffleChoices(String[] choices) {
+	List<String> choicesList = Arrays.asList(choices);
+	Collections.shuffle(choicesList);
+	return choicesList.toArray(new String[0]);
+}
 
-    public String getExplication() {
-        return explication;
-    }
+public String getQuestion() {
+	return question;
+}
 
-    public String[] getChoices() {
-        return choices;
-    }
+public String getExplication() {
+	return explication;
+}
 
-    public Boolean checkAnswer(String answer) {
-        if (answer.equals(this.answer)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+public String[] getChoices() {
+	return choices;
+}
+
+public Boolean checkAnswer(String answer) {
+	if (answer.equals(this.answer)) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 }
